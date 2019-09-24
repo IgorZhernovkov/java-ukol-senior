c698928dc16fa3280ca13f30731a9321f001eeac
Igor Zhernovkov <igor.jernovkov@dz.ru>
2019-09-24 18:58:50 +0500

    Added Swagger2 support

--- src/main/java/com/etnetera/hr/configuration/ApplicationConfiguration.java
+++ src/main/java/com/etnetera/hr/configuration/ApplicationConfiguration.java
@@ -11,11 +11,17 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Configuration of Spring beans
  */
 @Configuration
+@EnableSwagger2
 public class ApplicationConfiguration {
 
     /**
@@ -61,5 +67,14 @@ public class ApplicationConfiguration {
     @Bean(name = {"jscriptFrameWork"})
     public FieldsDefinition jscriptFrameworkFieldsDefinition() {
         return new JScriptFrameworkFieldsDefinition();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
     }
 }
